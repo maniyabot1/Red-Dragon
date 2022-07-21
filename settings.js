6993d522a73cea26c40c8f25ab3cff7ebb058730
maniyabot1 <109734373+maniyabot1@users.noreply.github.com>
2022-07-21 16:28:52 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,28 +22,28 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94755289855'] //Owner number in aive msg
+global.premium = ['+94755289855'] //Owner Number info
+global.ownernomer = '+94755289855' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎MANIYA BOT' //Owner Name
+global.botname = ' ☬MANIYABOT࿐' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ Youtube ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://www.youtube.com/channel/UCIFoVCegHrfh5BxoMSewChw' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬MANIYABOT࿐' //Bot Pacage Name
+global.author = '😎MANIYA❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_MANIYA Official.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://www.youtube.com/channel/UCIFoVCegHrfh5BxoMSewChw' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
